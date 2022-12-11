42f5b5d939314528237f1623a72d3e9afe8d961e
Ori Darshan <77589259+oridarshan@users.noreply.github.com>
2022-12-11 22:16:59 +0200

    comment Renter car lists views

--- app/src/main/java/com/rosenberg/uni/Renter/RenterCarViewFragment.java
+++ app/src/main/java/com/rosenberg/uni/Renter/RenterCarViewFragment.java
@@ -28,7 +28,10 @@ import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.List;
 
-
+/**
+ * this class is the code that works with the fragment_renter_car_view.xml Window
+ * here the user can see the vehicles he can rent
+ */
 public class RenterCarViewFragment extends Fragment {
 
     private List<Car> cars;
@@ -40,20 +43,27 @@ public class RenterCarViewFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_renter_car_view, container, false);
     }
 
+    /**
+     * show cars for rent
+     * @param view - View object of the window (hold the objects of texts inputs that screened)
+     * @param savedInstanceState -  last state of this fragment,should be null
+     */
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        // start and end dates for filtering
         EditText start = view.findViewById(R.id.renter_car_view_start_date);
         EditText end = view.findViewById(R.id.renter_car_view_end_date);
         Button filter = view.findViewById(R.id.renter_car_view_filter);
 
+        // list of cars to rent
         ListView carsView = view.findViewById(R.id.renter_car_view_list_view);
         List<String> carStrings = new ArrayList<>();
 
         FirebaseFirestore fs = FirebaseFirestore.getInstance();
         fs.collection("cars")
-                .whereEqualTo("renterID", null)
+                .whereEqualTo("renterID", null) // show only cars that are not reserved
                 .get()
                 .addOnSuccessListener(queryDocumentSnapshots -> {
                     cars = queryDocumentSnapshots.toObjects(Car.class);
@@ -63,14 +73,14 @@ public class RenterCarViewFragment extends Fragment {
                             cars.toArray(new Car[0]));
                     carsView.setAdapter(adapter);
                 });
-
+        // filtering process
         filter.setOnClickListener(v -> {
             long start_date_stamp,end_date_stamp;
-            if(start.getText().toString().isEmpty()){
+            if(start.getText().toString().isEmpty()){ // get start date for filtering
                 start_date_stamp =0;
                 Calendar calendar = new GregorianCalendar(0,1,1);
                 start_date_stamp = calendar.getTimeInMillis();
-            }else{
+            }else{ // parse start date for filtering
                 String [] splitdate = start.getText().toString().split("/");
                 Calendar calendar = new GregorianCalendar(Integer.parseInt(splitdate[2]),
                         Integer.parseInt(splitdate[1]),
@@ -78,26 +88,26 @@ public class RenterCarViewFragment extends Fragment {
                 start_date_stamp = calendar.getTimeInMillis();
             }
 
-            if(end.getText().toString().isEmpty()){
+            if(end.getText().toString().isEmpty()){ // get end date for filtering
                 Calendar calendar = new GregorianCalendar(3000,1,1);
                 end_date_stamp = calendar.getTimeInMillis();
-            }else{
+            }else{ // parse end date for filtering
                 String [] splitdate = end.getText().toString().split("/");
                 Calendar calendar = new GregorianCalendar(Integer.parseInt(splitdate[2]),
                         Integer.parseInt(splitdate[1]),
                         Integer.parseInt(splitdate[0]));
                 end_date_stamp = calendar.getTimeInMillis();
             }
+            // get only cars with end-of-rent time between start and end date
             fs.collection("cars")
                     .whereLessThan("endDateStamp",end_date_stamp)
                     .whereGreaterThan("endDateStamp",start_date_stamp)
+//                    .whereEqualTo("renterID", null) // show only cars that are not reserved TODO uncomment
                     .get()
                     .addOnSuccessListener(queryDocumentSnapshots -> {
                         cars = queryDocumentSnapshots.toObjects(Car.class);
                         Log.d("CAR_VIEW Renter","ADDING CARS" + cars.size());
-
-
-
+                        // show relevant cars
                         ArrayAdapter adapter = new ListItemCarViewAdapter(getActivity(),
                                 cars.toArray(new Car[0]));
                         carsView.setAdapter(adapter);
@@ -108,9 +118,10 @@ public class RenterCarViewFragment extends Fragment {
                 android.R.layout.simple_list_item_1,carStrings);
         carsView.setAdapter(adapter);
 
+        // car in index i selected
         carsView.setOnItemClickListener((adapterView, view1, i, l) -> {
             FragmentManager fm = getParentFragmentManager();
-
+            // car details and renting fragment
             Log.d("RenterCarView","sending to this id " + cars.get(i).getDocumentId());
             fm.beginTransaction().replace(R.id.main_fragment,
                             RenterCarViewDetailsFragment.newInstance(cars.get(i).getDocumentId()),
