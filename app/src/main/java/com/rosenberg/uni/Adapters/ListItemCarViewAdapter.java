efe3792c610759a2a4536a560184fe670cbea7e1
amiramir96 <89981387+amiramir96@users.noreply.github.com>
2022-12-12 15:32:50 +0200

    comments adpaters

--- app/src/main/java/com/rosenberg/uni/Adapters/ListItemCarViewAdapter.java
+++ app/src/main/java/com/rosenberg/uni/Adapters/ListItemCarViewAdapter.java
@@ -14,30 +14,42 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.rosenberg.uni.Entities.Car;
 import com.rosenberg.uni.R;
 
+/**
+ * this adapter purpose is to convert entry of car
+ * to row view that we can represent
+*/
 public class ListItemCarViewAdapter extends ArrayAdapter {
     private Car [] cars;
-    private String userID;
     private Activity context;
+
+    /**
+     * constructor
+     * @param context - of the activity
+     * @param cars - all cars for the table that the adapter would present only one row in the table
+     */
     public ListItemCarViewAdapter(Activity context, Car[] cars){
         super(context, R.layout.list_item_car_view,cars);
 
-        userID = FirebaseAuth.getInstance().getUid();
         this.context=context;
         this.cars = cars;
     }
 
     /**
      * Get view with details for car in index `position`
-     * @param position
-     * @param convertView
-     * @param parent
-     * @return
+     * @param position - for a car in a row in cars[]
+     * @param convertView - built in, we wont use
+     * @param parent - same above
+     * @return the row view object of a spesific entry of car
      */
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         LayoutInflater inflater = context.getLayoutInflater();
+
+        // make that row be exist
         View rowView=inflater.inflate(R.layout.list_item_car_view,null,true);
+
+        // var of text, will present the fields of a specific car
         TextView make = rowView.findViewById(R.id.list_item_car_view_make);
         TextView model = rowView.findViewById(R.id.list_item_car_view_model);
         TextView mileage = rowView.findViewById(R.id.list_item_car_view_milage);
@@ -52,6 +64,7 @@ public class ListItemCarViewAdapter extends ArrayAdapter {
             status.setText("Active");
         }
 
+        // init its values
         make.setText(cars[position].getMake());
         model.setText(cars[position].getModel());
         mileage.setText(" "+cars[position].getMileage().toString());
--- app/src/main/java/com/rosenberg/uni/Adapters/ListItemUserViewAdapter.java
+++ app/src/main/java/com/rosenberg/uni/Adapters/ListItemUserViewAdapter.java
@@ -15,27 +15,46 @@ import com.rosenberg.uni.Entities.Car;
 import com.rosenberg.uni.Entities.User;
 import com.rosenberg.uni.R;
 
+/**
+ * this adapter purpose is to convert entry of user
+ * to row view that we can represent
+ */
 public class ListItemUserViewAdapter extends ArrayAdapter {
+
     private User [] users;
     private Activity context;
+
+    /**
+     * constructor
+     * @param context - of the activity
+     * @param users - all users for the table that the adapter would present only one row in the table
+     */
     public ListItemUserViewAdapter(Activity context, User[] users){
         super(context, R.layout.list_item_car_view,users);
-
-        this.context=context;
+        this.context = context;
         this.users = users;
     }
 
+    /**
+     *
+     * @param position - for a user in a row in users[]
+     * @param convertView - built in, we wont use
+     * @param parent - same above
+     * @return the row view object of a spesific entry of user
+     */
     @SuppressLint("SetTextI18n")
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        LayoutInflater inflater = context.getLayoutInflater();
-        View rowView=inflater.inflate(R.layout.list_item_user_view,null,true);
+        LayoutInflater inflater = context.getLayoutInflater(); // init layout
 
-        TextView name = rowView.findViewById(R.id.list_item_user_name);
+        // make that row be exist
+        View rowView = inflater.inflate(R.layout.list_item_user_view,null,true);
 
-        name.setText(users[position].getFirstName()+" "+users[position].getLastName());
-//        mail.setText(users[position].getMail());
+        // var of text, will present the name of user
+        TextView userName = rowView.findViewById(R.id.list_item_user_name);
+        // init its values
+        userName.setText(users[position].getFirstName()+" "+users[position].getLastName());
 
         return rowView;
     }
