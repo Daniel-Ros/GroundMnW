d9619340c5e56e179a8d7afa504414fdca048fbe
amiramir96 <89981387+amiramir96@users.noreply.github.com>
2022-12-27 17:02:24 +0200

    starting to work on notification and handled bug of view profile (smthing dumb)

--- app/src/main/java/com/rosenberg/uni/Login/ViewProfileFragment.java
+++ app/src/main/java/com/rosenberg/uni/Login/ViewProfileFragment.java
@@ -142,7 +142,6 @@ public class ViewProfileFragment extends Fragment {
      */
     public void show(User user) {
         // init texts via user details
-        System.out.println(user.getId() +" not null\n");
         String userGender, userRole;
         if (user.getTenant()){
             userRole = "Tenant";
